246d149fc58f961d54cd99f021dcb21bfd4fd129
thaothao050192 <thaothao050192@gmail.com>
2024-10-14 11:38:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.on("callback_query", function (query) {
         bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
     } else {
         queries[query.id] = query;
-        let gameurl = "https://thaonm0501.github.io/nft_tele/";
+        let gameurl = "https://thaonm0501.github.io/testwebapp/";
         bot.answerCallbackQuery({
             callback_query_id: query.id,
             url: gameurl
